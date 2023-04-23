4b012d8c4dee68fcde78b2cb92c4552289d5a7d2
OleksandrYatskiv <yatsk1660@gmail.com>
2023-04-23 12:32:31 +0200

    Finished work

---
+++ index.js
@@ -0,0 +1,16 @@
+// first part of the homework
+firstString = prompt('What is your first string?');
+secondString = prompt('What is your second string?');
+thirdString = prompt('What is your third string?');
+alert(`${firstString} ${secondString} ${thirdString}`);
+
+
+// Second part of the homework
+fiveDigitNumber = prompt('What is your five-digit number?');
+firstNumber = fiveDigitNumber.slice(0,1);
+secondNumber = fiveDigitNumber.slice(1,2);
+thirdNumber = fiveDigitNumber.slice(2,3);
+fourthNumber = fiveDigitNumber.slice(3,4);
+fifthNumber = fiveDigitNumber.slice(4,5);
+
+alert(`${firstNumber} ${secondNumber} ${thirdNumber} ${fourthNumber} ${fifthNumber}`);
